711c2be40c9d7f090c2c36fdbf9c6ad5626ec8fa
HP <31686938+yougo96@users.noreply.github.com>
2024-07-15 17:32:54 +0200

    Filters systeme

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -1,23 +1,68 @@
 // === Fetch /works ========================================================================
-try {
-
-fetch("http://localhost:5678/api/works").then((res) => {return res.json()}).then((obj) => {
+async function works() {
+    fetch("http://localhost:5678/api/works").then((res) => {return res.json()}).then((obj) => {
 
         console.log(obj)
-        let galleryDiv = document.querySelector('.gallery')
+        let parentDiv = document.querySelector('.gallery')
         obj.forEach(i => {
-            let workDiv = document.createElement("figure")
-            workDiv.innerHTML = `
-            <figure>
+            let childDiv = document.createElement("figure")
+            childDiv.id = i.id
+            childDiv.setAttribute('categoryid', i.categoryId)
+            childDiv.setAttribute('userid', i.userId)
+            childDiv.innerHTML = `
                 <img src="${i.imageUrl}" alt="${i.title}">
                 <figcaption>${i.title}</figcaption>
-            </figure>
             `
-            galleryDiv.appendChild(workDiv)
+            parentDiv.appendChild(childDiv)
         });
-})
+    })
+}
+
+// === Fetch /categories ========================================================================
+async function categories() {
+    await fetch("http://localhost:5678/api/categories").then((res) => {return res.json()}).then((obj) => {
+
+        console.log(obj)
+        let parentDiv = document.querySelector('#portfolio > ul')
+        obj.forEach(i => {
+            let childDiv = document.createElement("li")
+            childDiv.id = i.id
+            childDiv.innerHTML = `
+            ${i.name}
+            `
+            parentDiv.appendChild(childDiv)
+        });
+    })
+
+    document.querySelectorAll("#portfolio > ul > li").forEach((elem) => {
+        elem.addEventListener("click", clickFilters)
+    })
+}
+
+function clickFilters(event) {
+    let currentFilterId = 0
+
+    document.querySelectorAll("#portfolio > ul > li").forEach((elem) => {elem.classList.toggle("active", false)})
+    event.target.classList.toggle("active", true);
+    currentFilterId = event.target.id
+       
+    document.querySelectorAll(".gallery > figure").forEach((elem) => {
+        elem.classList.toggle("d-none", true)
+
+        if (currentFilterId == 0) {
+            elem.classList.toggle("d-none", false)
+        }
+        else if (elem.getAttribute("categoryid") == currentFilterId) {
+            elem.classList.toggle("d-none", false)
+        }
+    })
 
 }
-catch(error) {
-    console.error("ERROR : ", error);
-}
+
+
+
+// === Build page ========================================================================
+works()
+categories()
+
+
